ae30c9ca6cd958f50484d7c3d5341789ae2f8d45
Eivind Mikael Lindbråten <eivind.lindbraten@gmail.com>
2017-08-25 12:32:27 +0200

    Greatly imrpoved update.sh and updated brew and caskfile.

--- apps/homebrew/_install.sh
+++ apps/homebrew/_install.sh
@@ -6,6 +6,9 @@ if [[ $? != 0 ]] ; then
     /usr/bin/ruby -e "$(curl -fsSL https://raw.github.com/gist/323731)"
 fi
 
+# Add taps
+brew tap buo/cask-upgrade
+
 # Installing brews and casks from Brewfile and Caskfile
 cat Brewfile | xargs brew install
 cat Caskfile | xargs brew cask install
--- update.sh
+++ update.sh
@@ -2,10 +2,41 @@
 
 # Updates all software components of the system.
 
+# Lists outdated updates and updates core macOS
+softwareupdate --list
 softwareupdate --install --all
+
+# Updates local package list, lists outdated packages and upgrades them. Then does a cleanup.
 brew update
+brew outdated
 brew upgrade
+brew cleanup -s
+# Diagnostics for a healty system
+brew doctor
+brew missing
+# Brew cask
+brew cask outdated
+brew cu --all --cleanup
+brew cask cleanup
+
+# Mas: List outdated App Store apps and update them
+mas outdated
 mas upgrade
+
+# Update global npm packages. Node and npm itself are updated through brew.
+brew outdated -g
 npm update -g
-gem update —system
-gem update
+
+# List outdated gems, update RubyGems itself, update installed gems. Cleanup.
+sudo gem outdated
+sudo gem update --system
+sudo gem update
+sudo gem cleanup
+
+# Update atom plugins without any questions
+apm upgrade -c false
+
+# Update latex pacakge manager and its packages
+sudo tlmgr update --self
+sudo tlmgr update --list
+sudo tlmgr update --all
