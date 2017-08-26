d975f23eb029f5d069a3ca2438054cea8ef5e9c8
Eivind Mikael Lindbråten <eivind.lindbraten@gmail.com>
2017-08-26 12:54:13 +0200

    Updated bre and cask file

--- apps/homebrew/_install.sh
+++ apps/homebrew/_install.sh
@@ -7,7 +7,10 @@ if [[ $? != 0 ]] ; then
 fi
 
 # Add taps
+# For cask-upgrade functionality. 'brew cu'
 brew tap buo/cask-upgrade
+# For brew services functionality 'brew services start mysql'
+brew tap homebrew/services
 
 # Installing brews and casks from Brewfile and Caskfile
 cat Brewfile | xargs brew install
