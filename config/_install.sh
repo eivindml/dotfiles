5420e03033fc78de887ad64a4afdbe7a685a55da
Eivind Mikael Lindbråten <eivind.lindbraten@gmail.com>
2017-09-16 11:08:01 +0200

    Remove atom and standard text editor and replace with Sublime Text

--- config/_install.sh
+++ config/_install.sh
@@ -9,3 +9,7 @@ sudo rm /private/etc/apache2/extra/httpd-vhosts.conf
 cd apache
 sudo ln httpd-vhosts.conf /private/etc/apache2/extra/httpd-vhosts.conf
 cd ../
+
+sudo rm -rf ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
+cd ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/
+ln -s ~/Documents/dotfiles/config/sublime User
