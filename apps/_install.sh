ba71c366216c558251993834f5f0eaa0de0cb156
Eivind Mikael Lindbråten <eivindml@icloud.com>
2018-09-21 10:06:50 +0200

    Sort series, and simplify install script

--- apps/_install.sh
+++ apps/_install.sh
@@ -4,47 +4,15 @@
 # TODO: Add comment support for all files
 # TODO: Get highlighting of files, and add icons to them
 
-# Homebrew Brews & Casks
-################################################################################
-
+# Install Homebrew
 which -s brew
 if [[ $? != 0 ]] ; then
 	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-# Installing brews and casks
-cat Brewfile | xargs brew install
-cat Caskfile | xargs brew cask install
-
-# Mac App Store Apps
-################################################################################
-
-which -s mas
-if [[ $? != 0 ]] ; then
-  echo "Install mas with homebrew first. ☠️"
-  return
-fi
-
+# Install brews, casks, macos apps, node packages and gems
+grep "^[^# ]" Brewfile | xargs brew install
+grep "^[^# ]" Caskfile | xargs brew cask install
 grep "^[^# ]" Masfile | xargs mas install
-
-# Node Pacakges
-################################################################################
-
-which -s node
-if [[ $? != 0 ]] ; then
-	echo "Install node with homebrew first. ☠️"
-	return
-fi
-
-cat Npmfile | xargs npm install -g
-
-# Ruby Gems
-################################################################################
-
-which -s gem
-if [[ $? != 0 ]] ; then
-    echo "Install ruby/gem with homebrew first. ☠️"
-    return
-fi
-
-cat Gemfile | xargs sudo gem install
+grep "^[^# ]" Npmfile | xargs npm install -g
+grep "^[^# ]" Gemfile | xargs sudo gem install
--- macos/terminal.sh
+++ macos/terminal.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# Set default shell to fish
+chsh -s /usr/local/bin/fish
+
 # Change computer name
 sudo scutil --set HostName macbook
