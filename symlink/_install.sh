1b738e83d4088a5caba37f175ae722338c1278a9
Eivind Mikael Lindbråten <eivindml@icloud.com>
2018-09-21 09:47:13 +0200

    Cleanup and refactoring

--- symlink/_install.sh
+++ symlink/_install.sh
@@ -2,13 +2,11 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# TODO: Automate this. Maybe have an array with source + dest string (can assume dest string if not present)
 
 ln -f $DIR/.gitconfig ~/.gitconfig
 ln -f $DIR/.gitignore_global ~/.gitignore_global
-ln -f $DIR/.hushlogin ~/.hushlogin
-ln -f $DIR/.lftprc ~/.lftprc
 ln -f $DIR/.vimrc ~/.vimrc
-ln -f $DIR/.zshrc ~/.zshrc
 ln -sfn $DIR/.flexget/ ~/.flexget
 ln -sfn $DIR/.vim/ ~/.vim
 ln -sfn $DIR/fish/ ~/.config/fish
