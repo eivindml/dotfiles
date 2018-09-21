97df30499c2866d6cee22dce50b5e84a077c4b28
Eivind Mikael Lindbråten <eivindml@icloud.com>
2018-09-21 11:14:52 +0200

    Add CLI arguments to script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
+#
+# Description:  Minimal boostrap script for dotfiles.
+#               Install and update apps and dotfiles to the system.
+# Author:       Eivind Mikael Lindbråten
+# Email:        eivindml@icloud.com
+# Github:       github.com/eivindml
+#
+# Usage:
+## dotfiles Bootstrap.
+##
+## Usage:
+##   ./bootstrap.h -i
+##   ./bootstrap.h -u
+##
+## Options:
+##   -h      Show this screen.
+##   -i      Install dotfiles to system.
+##   -u      Updates all software packages on system.
+
 
 # TODO: Add nice printouts throughout, and remove unwanted printouts. Maybe just a loading spinner for each step.
 # TODO: Ask for sudo as first thing, so we don't need it later on.
 # TODO: Add nice comments
+# TODO: Remove install and run install title and commands.
 
 install_title() {
 	read -r -p "Do you want to update $1? [y/N] " response
@@ -20,7 +40,6 @@ install_commands() {
 		eval $line
 	done
 }
-
 
 run_install() {
   # TODO: Do this in a cleaner way.
@@ -93,37 +112,40 @@ fi
 }
 
 run_help() {
-cat << EOF
-dotfiles Bootstrap.
-
-Usage:
-  ./bootstrap.h -i
-  ./bootstrap.h -u
-
-Options:
-  -h      Show this screen.
-  -i      Install dotfiles to system.
-  -u      Updates all software packages on system.
-EOF
+  # This parses the usage description from the comment
+  # header. Parses lines starting with double hashtags.
+  sed -n '/^##/,/^$/s/^## \{0,1\}//p' $0
 }
 
+
+# Parse command line arguments and do corresponding action.
 while getopts ":hiu" opt; do
   case $opt in
+
+    # Help option
     h)
       run_help
       ;;
+
+    # Install option
     i)
       run_install
       ;;
+
+    # Update option
     u)
       run_update
       ;;
+
+    # Invalid argument. Show help
     \?)
       run_help
       ;;
+
   esac
 done
 
+# No arguments specified, show help
 if (($# == 0)); then
   run_help
 fi
