e19ab0b7fdec444d8cdc90b6c2a3bb94d2d2a1f8
Eivind Mikael Lindbråten <eivindml@icloud.com>
2018-09-21 18:08:53 +0200

    Add greeting to fish, and change default start directory

--- symlink/fish/functions/fish_greeting.fish
+++ symlink/fish/functions/fish_greeting.fish
@@ -1,3 +1,13 @@
 function fish_greeting
-  printf "Good morning! ☀️\n\n"
+  set tod (date +"%H")
+  if test $tod -lt 12 -a $tod -gt 5
+    printf "Good morning! ☀️"
+  else if test $tod -lt 17
+    printf "Good afternoon! 🌍"
+  else if test $tod -lt 21
+    printf "Good evening! 🌗"
+  else
+    printf "Good night! 🌒"
+  end
+  printf "\n\n"
 end
