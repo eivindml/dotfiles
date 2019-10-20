4cbeb8c505e4871e6302287e8e444b748ace4611
Eivind Mikael Lindbråten <eivindml@icloud.com>
2019-10-20 17:10:53 +0000

    Move to swift based scripts

--- macos.sh
+++ macos.sh
@@ -1,41 +1,21 @@
 # Change computer name
-sudo scutil --set HostName macbook
-
-# Disable the App Store origin security
-sudo spctl --master-disable
-
-# Remove thick scrollbar
-defaults write NSGlobalDomain AppleShowScrollBars -string "WhenScrolling"
-
+scutil --set HostName macbook
 # Automatically quit printer app once the print jobs complete
 defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
-
 # Disable the “Are you sure you want to open this application?” dialog
 defaults write com.apple.LaunchServices LSQuarantine -bool false
-
-# Change desktop background
-osascript -e 'tell application "Finder" to set desktop picture to POSIX file "/Library/Desktop Pictures/Sierra 2.jpg"'
-
-# Change defalt finder window
-defaults write com.apple.finder NewWindowTarget -string "PfLo"
-defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}/"
-
 # Don't show icons for hard drives, servers, and removable media on the desktop
 defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool false
 defaults write com.apple.finder ShowHardDrivesOnDesktop -bool false
 defaults write com.apple.finder ShowMountedServersOnDesktop -bool false
 defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool false
-
 # Enable fullscreen for Simulator
-defaults write com.apple.iphonesimulator AllowFullscreenMode -bool YES
-
+defaults write com.apple.iphonesimulator AllowFullscreenMode -bool true
 # View full URL i status bar
 defaults write com.apple.Safari ShowFullURLInSmartSearchField -bool true
-
 # Enable the Develop menu and the Web Inspector in Safari
 defaults write com.apple.Safari IncludeDevelopMenu -bool true
 defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
 defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled -bool true
-
 # Add a context menu item for showing the Web Inspector in web views
 defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
