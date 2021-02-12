1a81608db5c1e5d03f9473797bd53f86a02bd03e
Eivind Mikael Lindbråten <eivindml@icloud.com>
2021-02-12 10:23:14 +0100

    Added transmission unrar script

--- scripts/transmission-unrar.sh
+++ scripts/transmission-unrar.sh
@@ -1,4 +1,8 @@
-#!/bin/bash
-#A simple script to extract a rar file inside a directory downloaded by Transmission.
-#It uses environment variables passed by the transmission client to find and extract any rar files from a downloaded torrent into the folder they were found in.
-find /$TR_TORRENT_DIR/$TR_TORRENT_NAME -name "*.rar" -execdir unrar e -o- "{}" \;
+#!/bin/zsh
+echo "------" >> /Users/eivindml/Library/Logs/transmission.log
+date >> /Users/eivindml/Library/Logs/transmission.log 
+echo "$TR_TORRENT_DIR" >> /Users/eivindml/Library/Logs/transmission.log
+echo "$TR_TORRENT_NAME" >> /Users/eivindml/Library/Logs/transmission.log
+echo "Trying to extract: $TR_TORRENT_DIR/$TR_TORRENT_NAME" >> /Users/eivindml/Library/Logs/transmission.log
+# find "$TR_TORRENT_DIR/$TR_TORRENT_NAME" -type d -maxdepth 1 -exec /bin/zsh -c 'cd "{}" && find . -name "*.rar" | xargs /usr/local/bin/7z x' \; &>> /Users/eivindml/Library/Logs/transmission.log
+find "$TR_TORRENT_DIR/$TR_TORRENT_NAME" -maxdepth 1 -name "*.rar" -execdir /usr/local/bin/7z x {} \;  &>> /Users/eivindml/Library/Logs/transmission.log
